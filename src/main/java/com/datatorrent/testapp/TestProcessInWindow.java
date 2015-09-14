5795918a3f266b457e9ca6038c545aefe017dd38
Timothy Farkas <tim@datatorrent.com>
2015-09-14 16:32:58 -0700

    Initial commit

---
+++ src/main/java/com/datatorrent/testapp/TestProcessInWindow.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.datatorrent.testapp;
+
+import com.datatorrent.api.Context.OperatorContext;
+import com.datatorrent.api.DefaultInputPort;
+import com.datatorrent.api.DefaultOutputPort;
+import com.datatorrent.api.Operator;
+import com.datatorrent.api.Operator.IdleTimeHandler;
+
+import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class TestProcessInWindow implements Operator, IdleTimeHandler
+{
+  private transient Semaphore lock = new Semaphore(0);
+
+  public final transient DefaultInputPort<Double> out = new DefaultInputPort<Double>() {
+    @Override
+    public void process(Double t)
+    {
+      try {
+        lock.acquire();
+      } catch (InterruptedException ex) {
+        throw new RuntimeException(ex);
+      }
+
+      lock.release();
+    }
+  };
+
+  @Override
+  public void beginWindow(long l)
+  {
+    lock.release();
+  }
+
+  @Override
+  public void endWindow()
+  {
+    try {
+      lock.acquire();
+    } catch (InterruptedException ex) {
+      throw new RuntimeException(ex);
+    }
+  }
+
+  @Override
+  public void setup(OperatorContext cntxt)
+  {
+  }
+
+  @Override
+  public void teardown()
+  {
+  }
+
+  @Override
+  public void handleIdleTime()
+  {
+    try {
+      lock.acquire();
+    } catch (InterruptedException ex) {
+      throw new RuntimeException(ex);
+    }
+
+    lock.release();
+  }
+}
