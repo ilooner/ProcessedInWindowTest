160d291e4d5e02011fd3fc5c5cc0c73a6a48077a
Timothy Farkas <tim@datatorrent.com>
2015-09-15 10:30:09 -0700

    Sleeping in process tuple.

--- src/main/java/com/datatorrent/testapp/TestProcessInWindow.java
+++ src/main/java/com/datatorrent/testapp/TestProcessInWindow.java
@@ -58,6 +58,12 @@ public class TestProcessInWindow implements Operator
       } catch (InterruptedException ex) {
         throw new RuntimeException(ex);
       }
+      
+      try {
+        Thread.sleep(1);
+      } catch (InterruptedException ex) {
+        throw new RuntimeException(ex);
+      }
 
       lock.release();
     }
